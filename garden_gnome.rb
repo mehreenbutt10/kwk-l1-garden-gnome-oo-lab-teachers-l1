adf614729bbb6523a88b553e82eb9c6d11c87ac3
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 18:49:40 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,53 @@
 # Code your instances here
+class GardenGnome
+  def initialize(hat_color="red")
+    @personality = personality
+    @hat_color = hat_color
+  end
+  
+  def personality
+    @personality = "evil"
+  end
+ 
+  def hat_color
+    @hat_color 
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def age
+    @age
+  end
+  
+  def age=(age)
+    @age=age
+  end
+  
+  def gluten_allergy
+    @gluten_allergy
+  end
+  
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+  
+  def gnaw
+   "Gnawing on a tree!!!"
+  end
+  
+  def shout
+    "GNARLY!!!"
+  end
+  
+  def introduce_self
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+end
+
+
